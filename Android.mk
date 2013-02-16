0101d1e771f080716a0ccb071847bba0f77d7141
Bernd Giesecke <bernd@giesecke.tk>
2013-02-16 10:55:41 +0900

    Added missing Android.mk
    Change-Id: I039233d6b10d832af7494b9a1901397b34913272

---
+++ Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := 
+
+LOCAL_STATIC_JAVA_LIBRARIES := 
+
+# Only compile source java files in this apk.
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := romstats
+
+#LOCAL_SDK_VERSION := current
+
+include $(BUILD_PACKAGE)
